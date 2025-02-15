81249349fa3ef4f3231be807984decc2e162e946
Daniel Ostapenkov <84470259+DanOstap@users.noreply.github.com>
2025-02-15 22:49:17 +0100

    finish menu block

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,11 +4,19 @@ function  Main() {
     return (
         <div className="Main-overlay">
             <div className="Main-menu">
-                <h1>Coffee 21</h1>
-                <button>Home</button>
-                <button>Store</button>
-                <button>Log in</button>
-                <button>Sing Up</button>
+                <div className="Menu-Logo">
+                    <h1>Cafe Shop 21</h1>
+                </div>
+                <div className="Menu-buttons">
+                    <button>Home</button>
+                    <button>Store</button>
+                    <button>Log in</button>
+                    <button>Sing Up</button>
+                </div>
+                <div className="Menu-LogIn-SingUp">
+                    <button> Sign In</button>
+                    <button> Sign Up</button>
+                </div>
             </div>
         </div>
     );
